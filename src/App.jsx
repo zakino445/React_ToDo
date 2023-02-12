af633062a246b59c8a3de4097a1adcb17b8e4a87
zaki <zaki20020302@gamil.com>
2023-02-12 21:32:02 +0900

    docs: #2 TODOリストの構成

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のエリア</p>
+        <ul>
+          <li>初期</li>
+          <button>達成</button>
+          <button>諦める</button>
+        </ul>
+      </div>
+      <div>
+        <p>完了のエリア</p>
+        <ul>
+          <li>初期</li>
+          <button>もう一度</button>
+        </ul>
+      </div>
+    </>
+  );
 };
