5fcbd60a9083e367606459c79b19c128c23ab0e9
zaki <zaki20020302@gamil.com>
2023-02-12 21:44:31 +0900

    style: #3 ToDoリストにstyle追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import "./styles.css";
 
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のエリア</p>
-        <ul>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODOリスト</p>
+        <ul className="list-row">
           <li>初期</li>
           <button>達成</button>
           <button>諦める</button>
         </ul>
       </div>
-      <div>
-        <p>完了のエリア</p>
-        <ul>
+      <div className="complete-area">
+        <p className="title">完了のTODOリスト</p>
+        <ul className="list-row">
           <li>初期</li>
           <button>もう一度</button>
         </ul>
