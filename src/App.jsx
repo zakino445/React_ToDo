f9c2616d3f947dacc4cf174138d6c742ba4d0573
zaki <zaki20020302@gamil.com>
2023-02-14 17:47:35 +0900

    モックへの変更に追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./styles.css";
 
 export const App = () => {
   const [incompleteTodos, setIncompleteTodos] = useState(["ああああ", "いいいい"]);
+  const [completeTodos, setCompleteTodos] = useState(["いいいい"]);
   return (
     <>
       <div className="input-area">
@@ -11,24 +12,30 @@ export const App = () => {
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODOリスト</p>
-        <ul className="list-row">
+        <div>
           {incompleteTodos.map((todo) => {
             return (
-              <>
+              <ul key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>達成</button>
                 <button>諦める</button>
-              </>
+              </ul>
             );
           })}
-        </ul>
+        </div>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODOリスト</p>
-        <ul className="list-row">
-          <li>初期</li>
-          <button>もう一度</button>
-        </ul>
+        <div>
+          {completeTodos.map((todo) => {
+            return (
+              <ul key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>もう一度</button>
+              </ul>
+            );
+          })}
+        </div>
       </div>
     </>
   );
